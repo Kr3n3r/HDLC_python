41dc34ce4813e42e679cbbb011f8b026ab857423
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-01-12 10:24:58 +0100

    mejorado b2e5

--- b2u1_alternativas/login.py
+++ b2u1_alternativas/login.py
@@ -1,10 +1,10 @@
 from tkinter import *
-#defining login function
+#defino la función de login
 def login():
-    #getting form data
+    #recojo los valores de usuario y contraseña
     uname=username.get()
     pwd=password.get()
-    #applying empty validation
+    #valido los datos
     if uname=='' or pwd=='':
         message.set("Rellena los campos vacíos!!!")
     else:
@@ -12,35 +12,35 @@ def login():
        message.set("Has entrado al sistema!")
       else:
        message.set("Contraseña o usuario incorrecto!!!")
-#defining loginform function
+
+#defino la función que crea el formulario de forma gráfica
 def Loginform():
     global login_screen
     login_screen = Tk()
-    #Setting title of screen
+    #título de la pantalla
     login_screen.title("Formulario de login")
-    #setting height and width of screen
+    #tamaño de la pantalla
     login_screen.geometry("300x250")
-    #declaring variable
-    global  message;
+    login_screen.configure(background="#5CDB95")
+    #declarando las variables que se van a usar para recoger los datos
+    global message
     global username
     global password
     username = StringVar()
     password = StringVar()
     message=StringVar()
-    #Creating layout of login form
-    Label(login_screen,width="300", text="Introduce tus datos", bg="orange",fg="white").pack()
-    #Username Label
-    Label(login_screen, text="  Usuario").place(x=20,y=40)
-    #Username textbox
-    Entry(login_screen, textvariable=username).place(x=90,y=42)
-    #Password Label
-    Label(login_screen, text="Password").place(x=20,y=80)
-    #Password textbox
-    Entry(login_screen, textvariable=password ,show="*").place(x=90,y=82)
-    #Label for displaying login status[success/failed]
-    Label(login_screen, text="",textvariable=message).place(x=95,y=170)
-    #Login button
-    Button(login_screen, text="Enter", width=10, height=1, bg="orange",command=login).place(x=105,y=130)
+    #Diseñando el layout del formulario
+    Label(login_screen,width="300", text="Introduce tus datos", bg="#05386B",fg="#EDF5E1").pack()
+    #Label e input del usuario
+    Label(login_screen, text="  Usuario", background="#5CDB95").place(x=20,y=40)
+    Entry(login_screen, textvariable=username, bg="#EDF5E1").place(x=90,y=42)
+    #Label e input de la contraseña
+    Label(login_screen, text="Password", background="#5CDB95").place(x=20,y=80)
+    Entry(login_screen, textvariable=password ,show="*", bg="#EDF5E1").place(x=90,y=82)
+    #Label para los mensajes de [correcto/incorrecto/void]
+    Label(login_screen, text="",textvariable=message, background="#5CDB95", fg="#EDF5E1").place(x=75,y=170)
+    #Botón de Login("Enter") donde valida con la función programada anteriormente V
+    Button(login_screen, text="Enter", width=10, height=1, command=login, background="#05386B", fg="#EDF5E1").place(x=105,y=130)
     login_screen.mainloop()
 # #calling function Loginform
 # Loginform()
